023f9529d39b84777f902820f1d13841cea87f86
0ne-zero <pouria.khakpour9909@gmail.com>
2025-09-11 03:41:14 +0330

    Fix: npm run build errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 export default defineConfig({
-  base: "/rubitech-ui/", // or "/" for <username>.github.io
   plugins: [react()],
   resolve: { alias: { "@": path.resolve(__dirname, "src") } }
 });
