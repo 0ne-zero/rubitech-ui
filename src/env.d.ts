e168487dce586c406defee92759372fde10689d6
0ne-zero <pouria.khakpour9909@gmail.com>
2025-09-24 02:48:03 +0330

    Impl: new env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_SITE_NAME: string;
   readonly VITE_SITE_URL: string;
   readonly VITE_ANALYTICS_KEY?: string;
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_PROFILE_PICTURE_BASE_URL: string
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv;
