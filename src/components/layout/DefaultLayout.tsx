a5344750566e7e7742d0f15aa155ee56c1404ccd
0ne-zero <pouria.khakpour9909@gmail.com>
2025-09-30 21:34:27 +0330

    Fix: bug in import missing things

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Header } from "@/components/layout/Header";
-import { Footer } from "@/components/layout/Footer";
 export function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
