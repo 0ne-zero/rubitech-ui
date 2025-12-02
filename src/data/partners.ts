866f8ab4ce48a868e966a43c6e8359a40129ee39
0ne-zero <pouria.khakpour9909@gmail.com>
2025-12-02 10:24:37 +0330

    fix: images url

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -11,25 +11,25 @@ export type Partner = {
 
 export const partners: Partner[] = [
   {
-    logoUrl: `${import.meta.env.BASE_URL}/images/logos/rubikamp.jpg`,
+    logoUrl: `${import.meta.env.BASE_URL}images/logos/rubikamp.jpg`,
     name: "روبیکمپ",
     description: "بورسیه و فراهم‌سازی مسیر رشد برای نوجوانان بااستعداد.",
     url: "https://rubikamp.org/?utm_source=rubitech",
   },
   {
-    logoUrl: `${import.meta.env.BASE_URL}/images/logos/irancell.webp`,
+    logoUrl: `${import.meta.env.BASE_URL}images/logos/irancell.webp`,
     name: "ایرانسل",
     description: "تامین دسترسی به اینترنت نوجوانان.",
     url: "https://irancell.ir/?utm_source=rubitech",
   },
   {
-    logoUrl: `${import.meta.env.BASE_URL}/images/logos/maktabkhoneh.jpg`,
+    logoUrl: `${import.meta.env.BASE_URL}images/logos/maktabkhoneh.jpg`,
     name: "مکتب‌خونه",
     description: "تامین محتوای آموزشی و رهگیری مسیر رشد نوجوانان.",
     url: "https://maktabkhooneh.org/?utm_source=rubitech",
   },
   {
-    logoUrl: `${import.meta.env.BASE_URL}/images/logos/snapp-pay.jpg`,
+    logoUrl: `${import.meta.env.BASE_URL}images/logos/snapp-pay.jpg`,
     name: "اسنپ‌پی",
     description: "حامی مالی نوجوانان برای خرید لپ‌تاپ.",
     url: "https://snapppay.ir/?utm_source=rubitech",
