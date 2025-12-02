c67bb56f910c9516c9fe385ecfd91ff3695249bd
0ne-zero <pouria.khakpour9909@gmail.com>
2025-12-02 09:59:39 +0330

    fix: remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 export default defineConfig({
-  base: "/rubitech-ui/",
   plugins: [react()],
   resolve: { alias: { "@": path.resolve(__dirname, "src") } }
 });
